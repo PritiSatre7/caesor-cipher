85206e675ff86e71d22159c2d75debb1634c055b
PritiSatre7 <priti.satre@vaastav.tech>
2026-02-10 16:43:44 +0530

    write a function for caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,3 +2,13 @@
 
 Function for caesar_cipher.
 """
+
+def encrypt_caesar(text, shift):
+    if text == "":
+        return ""
+    else:
+        return chr(ord(text[0]) + shift) + encrypt_caesar(text[1:], shift)
+
+
+if __name__ == "__main__":
+    print(encrypt_caesar("Hello", 2))
