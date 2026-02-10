99e99ff40d2b9d68ebc02f7c397daa5904fdcfc8
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-10 15:40:22 +0530

    added code for decrypr_caesar

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,22 @@
-def encrypt_caesar (text : str , shift: int) -> str:
+def encrypt_caesar(text : str , shift: int) -> str:
     pass
 
+def decrypt_caesar(text: str, shift: int) -> str:
+    decrypted = ""
+
+    for ch in text:
+        if ch.isalpha():
+            if ch.isupper():
+                decrypted += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                decrypted += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            decrypted += ch
+
+    return decrypted
+
+
+if __name__ == "__main__":
+    text = "JGNNQ"
+    shift = 2
+print("Decrypted Output:", decrypt_caesar(text, shift))
