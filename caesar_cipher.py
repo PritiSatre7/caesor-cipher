e156b234806f5882d11eca875c56a666467978c7
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-16 00:39:37 +0530

    Edited the description for better understanding to the user
    added what function does , parameters, what it returns , and example also

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,13 +1,36 @@
 
 """
 Function for caesar_cipher.
+This function provides the encrypt and decrypt text using the Caesar cipher.
+Supports uppercase letters, lowercase letters, digits, and symbols.
 """
 
 def shift_text(text: str, shift: int) -> str:
     """
-        Common function to shift alphabetical characters in the text.
-        Positive shift is used for encryption and negative shift for decryption.
+           Shift characters in the input text by a specified amount.
+
+        This function handles:
+        - Uppercase letters (A-Z)
+        - Lowercase letters (a-z)
+        - Digits (0-9)
+        - Symbols (!@#$%^&*()_+-=[]{};:,.<>/?)
+        - Other characters (e.g., spaces, tabs)
+
+        Parameters:
+            text (str): Input string to be shifted.
+            shift (int): Number of positions to shift characters.
+                         Positive shift for encryption, negative for decryption.
+
+        Returns:
+            str: The resulting string after shifting all supported characters.
+
+         Example:
+        >>> shift_text("Hello 123!", 2)
+        'Jgnnq 345!'
+        >>> shift_text("Jgnnq 345!", -2)
+        'Hello 123!'
     """
+
     digits = "0123456789"
     symbols = "!@#$%^&*()_+-=[]{};:,.<>/?"
     result = ""
@@ -28,20 +51,36 @@ def shift_text(text: str, shift: int) -> str:
 
 def encrypt_caesar(text: str, shift: int) -> str:
     """
-        Encrypts the input text using Caesar cipher.
+        Encrypt text using Caesar cipher.
 
-        Each letter in the plaintext is shifted forward in the alphabet by the specified number of positions.
-         Non-alphabetic characters are not changed.
+        Parameters:
+            text (str): Plaintext to encrypt.
+            shift (int): Number of positions to shift characters forward.
+
+        Returns:
+            str: Encrypted text
+
+        Example:
+        >>> encrypt_caesar("HELLO 123!@", 2)
+        'JGNNQ 345#$'
     """
     return shift_text(text, shift)
 
 
 def decrypt_caesar(text: str, shift: int) -> str:
     """
-        Decrypts the encrypted text using Caesar cipher.
+        Decrypt text encrypted with Caesar cipher.
 
-        The Caesar cipher shifts each alphabetic character in the plaintext by a fixed number of positions.
-        This function reverses that shift to retrieve the original message.
+        Parameters:
+            text (str): Encrypted text.
+            shift (int): Number of positions originally used to shift characters.
+
+        Returns:
+            str: Decrypted (original) text.
+
+        Example:
+        >>> decrypt_caesar("JGNNQ 345#$", 2)
+        'HELLO 123!@'
     """
     return shift_text(text, -shift)
 
