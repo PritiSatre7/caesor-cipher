7e72086e7c78523ed3e19b9ec19e590fcdd85998
SamuGavhane <samruddhi.gavhane@vaastav.tech>
2026-02-15 20:08:33 +0530

    added only one function name to the file
    instead of two function added only one

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,49 +1,32 @@
 
 """
-
 Function for caesar_cipher.
 """
 
-def encrypt_caesar(text, shift):
-    """
-    This Function encrypts the input text using caesar cipher.
-    Each character is shifted by the given value.
-
-    :param text: The input text to be encrypted.
-    :param shift: Number of characters to shift.
-    :return: Encrypted text.
-
-    """
-
-    if text == "":
-        return ""
-    else:
-        return chr(ord(text[0]) + shift) + encrypt_caesar(text[1:], shift)
-
-def decrypt_caesar(text: str, shift: int) -> str:
-    """
-    Decrypts a text that was encrypted using the Caesar Cipher technique.
-
-    Each alphabetical character in the input text is shifted backward
-    by the giving shift value and Non-alphabetical characters remain unchanged.
-
-    :param text: The encrypted text to be decrypted
-    :param shift: The number of positions used during encryption
-    :return: The decrypted plain text
-    """
-    decrypted = ""
+def shift_text(text: str, shift: int) -> str:
+    result = ""
 
     for ch in text:
         if ch.isalpha():
             if ch.isupper():
-                decrypted += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+                result += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
             else:
-                decrypted += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+                result += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
         else:
-            decrypted += ch
+            result += ch
 
-    return decrypted
+    return result
+
+
+def encrypt_caesar(text: str, shift: int) -> str:
+    return shift_text(text, shift)
+
+
+def decrypt_caesar(text: str, shift: int) -> str:
+    return shift_text(text, -shift)
 
 
 if __name__ == "__main__":
-    print(encrypt_caesar("Hello", 2))
+    encrypted = encrypt_caesar("HELLO", 2)
+    print(encrypted)
+    print(decrypt_caesar(encrypted, 2))
